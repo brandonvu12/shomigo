8a4b45766786b818c3acd94ddaf0a783820e3750
Gavin Hudson <gavinchudson@gmail.com>
2019-07-23 16:28:38 -0700

    add login welcome

--- main.py
+++ main.py
@@ -13,15 +13,16 @@ class MainHandler(webapp2.RequestHandler):
     login_template = JINJA_ENVIRONMENT.get_template("login.html")
     user = users.get_current_user()
     if user:
+        self.response.write(login_template.render())
         nickname = user.nickname()
         logout_url = users.create_logout_url('/')
-        greeting = 'Welcome, {}! (<a href="{}">sign out</a>)'.format(
+        greeting = '(<a href="{}">sign out</a>)'.format(
         nickname, logout_url)
     else:
         login_url = users.create_login_url('/')
         greeting = '<a href="{}">Sign in</a>'.format(login_url)
     self.response.write(greeting)
-    self.response.write(login_template.render())
+
 
 
 
