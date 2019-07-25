7662607938e1849dc64e23be8f7824a278ce2633
Gavin Hudson <gavinchudson@gmail.com>
2019-07-25 10:43:27 -0700

    Fix if user doesnt search it blows up

--- main.py
+++ main.py
@@ -60,17 +60,22 @@ class List(webapp2.RequestHandler):
         list_template = JINJA_ENVIRONMENT.get_template("templates/list.html")
 #Gets the search text and gets it from the api
         user_search = self.request.get('user_search_html')
-        user_search =  user_search.replace(" ", "+")
-        url = 'https://api.themoviedb.org/3/search/tv?api_key=affe4b9cbbe43b30bf85a6ae31037c7d&query=%s' %(user_search)
-        result = urlfetch.fetch(url)
-        result_decoded = result.content.decode('utf-8')
-        result_json = json.loads(result_decoded)
-        result_show = result_json['results'][0]['name']
+        if not user_search:
+            self.response.write(list_template.render())
+        else:
+            user_search =  user_search.replace(" ", "+")
+            url = 'https://api.themoviedb.org/3/search/tv?api_key=affe4b9cbbe43b30bf85a6ae31037c7d&query=%s' %(user_search)
+            result = urlfetch.fetch(url)
+            result_decoded = result.content.decode('utf-8')
+            result_json = json.loads(result_decoded)
+            result_show1 = result_json['results'][0]['name']
+            result_date1 = result_json['results'][0]['first_air_date']
 #Sends it to the html file
-        result_dict = {
-            "show_return": result_show,
-        }
-        self.response.write(list_template.render(result_dict))
+            result_dict = {
+                "show_return1": result_show1,
+                "show_date1": result_date1,
+                }
+            self.response.write(list_template.render(result_dict))
 
 class Friends(webapp2.RequestHandler):
     def get(self):
